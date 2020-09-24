4caf210bd42d4cbb17f8ecef32a1a846d7d33583
Deit <lavless63@gmail.com>
2020-09-24 18:05:28 +0300

    method 2

--- random.cpp
+++ random.cpp
@@ -1,83 +1,73 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
 #include <stdlib.h>
 #include <ctime>
 
 using namespace std;
 
-/*long* Linear_method() {
+long* Linear_method() {
 	srand(time(NULL));
 	long X[1000];
 	long a=2, c=3, m= 9241;
-	int x0 = 1;
+	int x0 =rand();
 	X[0] = (a * x0 + c) % m;
 	for (int i = 0; i < 999; i++) {
-		X[i + 1] = (a * X[i] + c) % m;
+		X[i+1] = (a * X[i] + c) % m;
 	}
 	return X;
+}
 
+long* Square_method() {
+	srand(time(NULL));
+	long X[1000];
+	long d=5,a = 2, c = 3, m = 9241;
+	int x0 = rand();
+	X[0] = (d * x0 * x0 + a * x0 + c) % m;
+	for (int i = 0; i < 999; i++) {
+		X[i + 1] = (d * X[i] * X[i] + a * X[i] + c) % m;
+	}
+	return X;
 }
 
 
 void main(){
+
+	string interval[10];
+	interval[0] = "[0,0; 0,1]";
+	interval[1] = "[0,1; 0,2]";
+	interval[2] = "[0,2; 0,3]";
+	interval[3] = "[0,3; 0,4]";
+	interval[4] = "[0,4; 0,5]";
+	interval[5] = "[0,5; 0,6]";
+	interval[6] = "[0,6; 0,7]";
+	interval[7] = "[0,7; 0,8]";
+	interval[8] = "[0,8; 0,9]";
+	interval[9] = "[0,9; 1,0]";
+
+
 	int Method;
+	cout << "Enter Method numer from 1 to 10 "<<endl;
 	cin >> Method;
 	long* r;
+	if (Method <= 0) {
+		cout << "Please, enter number from 1 to 10";
+	}
 	if (Method == 1) {
 		r = Linear_method();
-		for (int i = 0; i < 1000; i++) {
-			cout << *(r + i)/(double)(9240)  << " ";
+		cout <<  "First 10 numbers: ";
+		for (int i = 0; i < 10; i++) {
+			
+			cout << *(r + i)/(double)(9241)  << " ";
+
+		}
+		cout << " " << endl;
+		for (int i = 0; i < 10; i++) {
+			
+			cout << interval[i]<<endl;
+
 		}
 	}
-}*/
-
-
-
-long* M1() {
-    long a = 2, c = 3, m = 7877;
-    int x0 = 1; long X[1000];
-    int i, j, k = 0;
-    X[0] = ((a * x0 + c) % m);
-    for (i = 0; i < 999; i++) {
-        X[i + 1] = ((a * X[i] + c) % m);
-    }
-    return X;
-}
-
-long* M2() {
-    long d = 1, a = 2, c = 3, m = 7877;
-    int x0 = 1; long X[10000];
-
-    X[0] = (d * x0 * x0 + a * x0 + c) % m;
-    for (int i = 0; i < 999; i++) {
-        X[i + 1] = (d * X[i] * X[i] + a * X[i] + c) % m;
-    }
-    return X;
 }
 
 
 
-
-void main() {
-    int Method_number = 0;
-    double freq;
-
-    cout << "0...10" << " ";
-    cin >> Method_number;
-    if (Method_number != 0) {
-        if (Method_number == 1) {
-            long* p;
-            p = M1();
-            for (int i = 0; i < 1000; i++) {
-                freq = *(p + i) / (double)(7876);
-                cout << freq;
-            }
-        }
-       /* if (Method_number == 2) {
-            long* p;
-            p = M2();
-            for (int i = 0; i < 1000; i++) {
-                cout << *(p + i) / (double)(7876) << " ";
-            }*/
-        }
-    }
